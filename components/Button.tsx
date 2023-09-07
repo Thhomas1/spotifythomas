c8005a8b34d030650820589a0417e72ded31470e
Thhomas1 <thomasromero10@hotmail.com>
2023-09-07 15:46:15 -0300

    button

---
+++ components/Button.tsx
@@ -0,0 +1,29 @@
+import {forwardRef} from "react";
+import { twMerge } from "tailwind-merge";
+
+
+interface ButtonProps
+    extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
+    className,
+    children,
+    disabled,
+    type = "button",
+    ...props
+}, ref) => {
+    return (
+        <button 
+        type={type}
+        className={twMerge(`
+        w-full
+        rounded-full`)}>
+
+        {children}
+        </button>
+    )
+})
+// 42:52
+Button.displayName = "Button";
+
+export default Button
